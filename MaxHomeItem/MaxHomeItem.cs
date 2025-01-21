6f246c3a35ce9735281f2fa1d89ab1dd4b82d44e
Jojy Guy <kidpowercraft@gmail.com>
2025-01-21 13:32:55 -0700

    added all mods
    but with a .gitignore this time. has Butter Stabs (BasSharpStab2), Classic Waves Plus, Critical Weakness, Less Forceful Punches, Max Home Item Changer, and yet to be released This is Mine

---
+++ MaxHomeItem/MaxHomeItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using UnityEngine;
+
+namespace MaxHomeItem
+{
+    public class MaxItemsHome : ThunderScript
+    {
+        internal class modOptions
+        {
+            [ModOptionSlider]
+            [ModOption(name = "Max Item Value", tooltip = "How many items can be saved to the home. Requires a level reload. Having too many items may cause performance issues and/or large save file sizes!", defaultValueIndex = 100, valueSourceName = nameof(ValueArray), order = 1)]
+            public static int MaxItemValue;
+
+            public static ModOptionInt[] ValueArray()
+            {
+                List<ModOptionInt> options = new List<ModOptionInt>();
+                int val = 0;
+                while (val <= 1000)
+                {
+                    options.Add(new ModOptionInt(val.ToString("0"), val));
+                    val += 1;
+                }
+                return options.ToArray();
+            }
+        }
+
+
+        private class MaxItemCode : ThunderScript
+        {
+            public override void ScriptEnable()
+            {
+                base.ScriptEnable();
+                EventManager.onLevelLoad += EventManager_onLevelLoad;
+            }
+
+            private void EventManager_onLevelLoad(LevelData levelData, LevelData.Mode mode, EventTime eventTime)
+            {
+                Catalog.gameData.pc.maxHomeItem = modOptions.MaxItemValue;
+                Debug.Log($"Set max saved items to {modOptions.MaxItemValue}");
+            }
+        }
+    }
+}
